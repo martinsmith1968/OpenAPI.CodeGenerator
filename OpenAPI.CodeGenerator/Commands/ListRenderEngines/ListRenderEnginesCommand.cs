fe3e2d3c0c2fb7444c85127d689047603c4abf59
Martin Smith <martinsmith1968@gmail.com>
2018-12-28 21:47:01 +0000

    Fixed up List Render Engines command

--- OpenAPI.CodeGenerator/Commands/ListRenderEngines/ListRenderEnginesCommand.cs
+++ OpenAPI.CodeGenerator/Commands/ListRenderEngines/ListRenderEnginesCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using OpenAPI.CodeGenerator.Common.Types;
 
 namespace OpenAPI.CodeGenerator.Commands.ListRenderEngines
@@ -11,12 +12,18 @@ namespace OpenAPI.CodeGenerator.Commands.ListRenderEngines
 
         public void Execute()
         {
-            var renderEngines = Enum.GetNames(typeof(RenderEngineType));
+            var renderEngines = Enum.GetNames(typeof(RenderEngineType))
+                                    .OrderBy(n => n)
+                                    .ToArray();
+
+            var indexSize = renderEngines.Count().ToString().Length;
 
             Console.WriteLine("Render Engines:");
+
+            var index = 0;
             foreach (var renderEngine in renderEngines)
             {
-                Console.WriteLine($"  {renderEngine}");
+                Console.WriteLine(string.Format("{0," + indexSize + "}: {1}", ++index, renderEngine));
             }
         }
     }
