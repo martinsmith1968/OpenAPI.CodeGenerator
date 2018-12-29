db6e2e26deb6d20d38e15e0aa936ee6974b47b49
Martin Smith <martinsmith1968@gmail.com>
2018-12-29 00:49:49 +0000

    Repaired after rebase

--- OpenAPI.CodeGenerator/Commands/CommandFactory.cs
+++ OpenAPI.CodeGenerator/Commands/CommandFactory.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-<<<<<<< HEAD
-using OpenAPI.CodeGenerator.Commands.Generate;
-using OpenAPI.CodeGenerator.Commands.ListRenderEngines;
-using OpenAPI.CodeGenerator.Commands.ListLanguages;
-=======
 using Autofac;
-using OpenAPI.CodeGenerator.Extensions;
 using OpenAPI.CodeGenerator.Interfaces;
->>>>>>> Factored for manual Ioc resolution
 
 namespace OpenAPI.CodeGenerator.Commands
 {
@@ -19,21 +12,9 @@ namespace OpenAPI.CodeGenerator.Commands
             if (command == null)
                 throw new ArgumentOutOfRangeException(nameof(commandType), $"Invalid or unsupported {nameof(CommandType)}: {commandType.ToString()}");
 
-<<<<<<< HEAD
-                case CommandType.ListLanguages:
-                    return new ListLanguagesCommand(args);
-
-                case CommandType.ListRenderEngines:
-                    return new ListRenderEnginesCommand(args);
-
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(commandType), $"Invalid or unsupported {nameof(CommandType)}: {commandType.ToString()}");
-            }
-=======
             command.SetArguments(args);
 
             return command;
->>>>>>> Factored for manual Ioc resolution
         }
     }
 }
