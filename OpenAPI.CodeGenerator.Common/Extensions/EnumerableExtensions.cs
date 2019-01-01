763e03dbe5807cab9abf9525d03ad545ffcd7f0e
Martin Smith <martinsmith1968@gmail.com>
2019-01-01 23:24:37 +0000

    Moved embedded templates around

--- OpenAPI.CodeGenerator.Common/Extensions/EnumerableExtensions.cs
+++ OpenAPI.CodeGenerator.Common/Extensions/EnumerableExtensions.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace OpenAPI.CodeGenerator.Extensions
+namespace OpenAPI.CodeGenerator.Common.Extensions
 {
     public static class EnumerableExtensions
     {
