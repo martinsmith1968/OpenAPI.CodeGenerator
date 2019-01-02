2f12e2ade9c47d4f5efd5da9498df402969a8cb4
Martin Smith <martinsmith1968@gmail.com>
2019-01-02 22:29:22 +0000

    Added JSON language

---
+++ OpenAPI.CodeGenerator.Language.JSON/JSONLanguage.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using OpenAPI.CodeGenerator.Common.Languages;
+using OpenAPI.CodeGenerator.Common.Types;
+
+namespace OpenAPI.CodeGenerator.Language.JSON
+{
+    public class JSONLanguage : BaseLanguage
+    {
+        public static JsonSerializerSettings Settings = new JsonSerializerSettings();
+
+        public override string FileExtension => "csv";
+
+        private string _definitionFileName;
+
+        public override string BuildOutputFileName(string itemName, TemplateItemType templateItemType)
+        {
+            if (templateItemType == TemplateItemType.Definition)
+            {
+                _definitionFileName = itemName;
+            }
+
+            return _definitionFileName;
+        }
+
+        public override void ApplyArguments(string[] args)
+        {
+            Settings = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                Converters = new List<JsonConverter>()
+                {
+                    new StringEnumConverter
+                    {
+                        NamingStrategy = new DefaultNamingStrategy()
+                    }
+                }
+            };
+        }
+    }
+}
